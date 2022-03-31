94ca8ab72309d5b482df939cb580edc839e2c8cd
Jhonatan <jhony.95.can@gmail.com>
2022-03-31 12:08:34 -0600

    Animation player added for the Player

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,15 +1,17 @@
 extends KinematicBody2D
 
-var screen_size
 var speed = 100
 var velocity = Vector2.ZERO
 const MAX_SPEED = 100
 const ACCELERATION = 400
 const FRICTION = 500
 
+onready var screen_size = get_viewport_rect().size
+onready var animationPlayer = $AnimationPlayer
+
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	screen_size = get_viewport_rect().size
+#func _ready():
+#	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -22,7 +24,9 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animationPlayer.play("RunRight")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationPlayer.play("IdleRight")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta) 
 	velocity = move_and_slide(velocity)
